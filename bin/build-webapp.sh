3fbb601b6f12ff464c7d3b85c377dc7fb02fc326
proper_rob <proper_rob@bah.com>
2017-07-21 13:00:01 -0400

    Adding a JUnit test to demonstrate unit testing in the pipeline

--- bin/build-webapp.sh
+++ bin/build-webapp.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+MAVEN_ARGS="$@"
+
 # https://hub.docker.com/_/maven/
 MAVEN_IMAGE="maven:3.5.0-jdk-8-alpine"
 
@@ -21,7 +23,9 @@ function get_absolute_path {
 REPO_DIR=$(get_parent $0)
 REPO_HOME=$(get_absolute_path "${REPO_DIR}")
 
-set -x
-docker pull "${MAVEN_IMAGE}"
-docker run -v "/${REPO_HOME}/webapp:/opt/project" "${MAVEN_IMAGE}" \
-     -c "ls -al; cd /opt/project; ls -al; maven clean package"
+WORK_DIR="/${REPO_HOME}/webapp"
+
+winpty docker run --name maven --rm \
+    --volume "/${REPO_HOME}/webapp:/project" \
+    "${MAVEN_IMAGE}" \
+    sh -c "cd /project; mvn ${MAVEN_ARGS} test"
--- webapp/src/main/java/com/bah/demos/helloworld/WelcomeController.java
+++ webapp/src/main/java/com/bah/demos/helloworld/WelcomeController.java
@@ -9,14 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WelcomeController {
 
+	public static final String MESSAGE = "Hello World";
+
 	// inject via application.properties
 	@Value("${welcome.message:test}")
-	private String message = "Hello World";
+	private String message = MESSAGE;
 
 	@RequestMapping("/")
 	public String welcome(Map<String, Object> model) {
 		model.put("message", this.message);
 		return "welcome";
 	}
-
 }
